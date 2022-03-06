7983f150045668b7ad1a1bfe874fa5b5f7546f83
ashishkanojia <ashish.kanojia@quantiphi.com>
2022-03-07 00:36:18 +0530

    updated Day1

---
+++ Days 1/day1.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator")
+print("What's the name of the city you grew in?")
+input1 = input()
+print("What is your pet name?")
+input2 = input()
+print("YOur brand name can be " + input1 + " " + input2)
