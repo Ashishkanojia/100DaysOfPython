8540fa571f81063a83ebea6deb6990b8059bf784
ashishkanojia <ashish.kanojia@quantiphi.com>
2022-03-07 00:37:00 +0530

    updated day 1

--- Days 1/day1.py
+++ Days 1/day1.py
@@ -4,3 +4,8 @@ input1 = input()
 print("What is your pet name?")
 input2 = input()
 print("YOur brand name can be " + input1 + " " + input2)
+
+// https://github.com/lenargasimov/100-days-of-python
+
+// https://github.com/lenargasimov/100-days-of-python
+
